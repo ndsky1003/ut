354da9d41529286f9f7d1aaf603668aa8ad6de93
mdadc <mdacc@acc.cc>
2023-03-28 10:51:42 +0700

    add

--- rand_test.go
+++ rand_test.go
@@ -1,25 +1,62 @@
 package rand
 
 import (
-	"fmt"
 	"testing"
 )
 
-func BenchmarkRand(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		v := GenID(6, func(id int) bool { return true })
-		if len(fmt.Sprintf("%d", v)) != 6 {
-			b.Error("dd")
-		}
+func TestGenID(t *testing.T) {
+	type args struct {
+		num_width uint8
+		canUse    func(id int) bool
+	}
+	tests := []struct {
+		name string
+		args func(t *testing.T) args
+
+		want1 int
+	}{
+		{
+			name: "test1",
+			args: func(t *testing.T) args {
+				return args{
+					num_width: 8,
+					canUse: func(id int) bool {
+						return true
+					},
+				}
+			},
+			want1: 12,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tArgs := tt.args(t)
+			got1 := GenID(tArgs.num_width, tArgs.canUse)
+			t.Log(got1)
+		})
 	}
 }
 
-func TestPick(t *testing.T) {
-	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	for i := 0; i < 100; i++ {
-		pick := i % 9
-		v := Pick[int](arr, pick)
-		t.Log(v)
+func BenchmarkGenID(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GenID(8, func(id int) bool {
+			return true
+		})
 	}
-	t.Error("333")
+}
+
+// 本来生成范围就是1->20来位，所以模糊测试一定失败
+func FuzzGenID(f *testing.F) {
+	testcases := []uint8{1, 1, 2, 3, 4, 5, 6}
+	for _, tc := range testcases {
+		f.Add(tc) // Use f.Add to provide a seed corpus
+	}
+	f.Fuzz(func(t *testing.T, orig uint8) {
+		t.Log(orig)
+		got1 := GenID(orig, func(id int) bool {
+			return true
+		})
+		t.Logf("out:%v", got1)
+	})
 }
